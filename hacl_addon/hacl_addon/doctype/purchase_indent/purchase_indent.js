cddd049042ff66a092246f4e950e411bfe20d4b1
Hidayat ali <136191740+Inshasis@users.noreply.github.com>
2023-06-11 01:57:37 +0530

    Doctype
    all doctype migrate local to custom app

---
+++ hacl_addon/hacl_addon/doctype/purchase_indent/purchase_indent.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Inshasis Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Purchase Indent', {
+	// refresh: function(frm) {
+
+	// }
+});
